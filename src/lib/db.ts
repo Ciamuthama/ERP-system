613ddfc310f92189d044d110358dcea11b15ca69
Peter Muthama <114055846+Ciamuthama@users.noreply.github.com>
2025-07-29 10:10:34 +0300

    Update db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -3,8 +3,8 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: "127.0.0.1",
   user: "root",
-  password: "123456",
-  database: "sacco_db",
+  password: "password",
+  database: "database",
   waitForConnections: true,
   connectionLimit: 10, 
   queueLimit: 0,
